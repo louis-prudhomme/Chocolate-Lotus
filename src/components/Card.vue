189ab1949a0f0a6d1b11b8fc65015ad4d61cd949
PrideTheDaemon <hellangel@live.fr>
2020-04-14 17:42:40 +0200

    Added placeholder and loading indicator to card components Fixed cards height Fixed cards image position                                                                                              Fixed Card height

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,11 +3,24 @@
         <v-card
         align-content="center"
         align="center"
-        height="100%">
+        height="100%"
+        raised>
             <v-img
             class="white--text align-end"
             :src="card.imageUrl"
-            height="200px"/>
+            height="376px"
+            lazy-src="https://www.magiclibrarities.net/rarities/alternate-4th-edition-normal-backside.jpg"
+            position="center top"
+            contain>
+                <template v-slot:placeholder>
+                    <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center">
+                        <v-progress-circular indeterminate color="grey lighten-5"/>
+                    </v-row>
+                </template>
+            </v-img>
             <v-card-title>{{ card.name }}</v-card-title>
             <v-card-subtitle>{{ card.type }}</v-card-subtitle>
             <v-card-text>{{ card.set }}</v-card-text>
