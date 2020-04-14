3b2fe4572fd69b945b7bce69088953b2d7fb8970
Mélanie <marquesmelanie5@gmail.com>
2020-04-14 23:30:37 +0200

    Refactor card display design

--- src/views/DisplayCard.vue
+++ src/views/DisplayCard.vue
@@ -1,3 +1,4 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
 <div>
     <v-card max-width="600">
@@ -15,7 +16,7 @@
                 </tr>
                 <tr>
                     <td>Color </td>
-                    <td>{{ card.colors }}</td>
+                    <td v-for="row in card.colors" v-bind:key="row"> <img :src="getColor(row)" v-bind:alt="row" height="30" width="30"/> </td>
                 </tr>
                 <tr>
                     <td>Type </td>
@@ -34,7 +35,30 @@
     <h1>Description: </h1>
     <p>{{card.text}}</p>
     <h1>Rules: </h1>
-    <p>{{card.rulings}}</p>
+
+    <v-timeline dense clipped>
+    <v-timeline-item
+      color="grey lighten-2"
+      fill-dot
+      right
+      v-for="rule in card.rulings" v-bind:key="rule"
+    >
+    <v-card width="700">
+        <v-card-title class="grey lighten-2">
+          <h2 class="display-1 white--text font-weight-light">{{rule.date}}</h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-icon size="50">mdi-calendar-text</v-icon>
+            <v-col cols="12" md="10">
+              <p>{{rule.text}}</p>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+    </v-timeline>
+
 </div>
 </template>
 
@@ -44,6 +68,18 @@ export default {
   name: 'DisplayCard',
   props: {
     card: Object
+  },
+
+  methods: {
+    getColor (color) {
+      var dictionnary = {}
+      dictionnary.Blue = require('@/assets/colors/blue.png')
+      dictionnary.Black = require('@/assets/colors/black.png')
+      dictionnary.Yellow = require('@/assets/colors/yellow.png')
+      dictionnary.Green = require('@/assets/colors/green.png')
+      dictionnary.Red = require('@/assets/colors/red.png')
+      return dictionnary[color]
+    }
   }
 }
 </script>
