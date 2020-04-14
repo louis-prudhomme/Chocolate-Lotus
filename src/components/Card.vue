975bb85d84c0c95ce01a78e4d3766076480ac98d
PrideTheDaemon <hellangel@live.fr>
2020-04-14 18:25:37 +0200

    Reformated card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,7 +15,9 @@
                     class="fill-height ma-0"
                     align="center"
                     justify="center">
-                        <v-progress-circular indeterminate color="grey lighten-5"/>
+                        <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"/>
                     </v-row>
                 </template>
             </v-img>
