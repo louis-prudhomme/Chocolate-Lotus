d67f04487cc88059781af9d3de7efde4c2282ed4
PrideTheDaemon <hellangel@live.fr>
2020-04-19 16:12:14 +0200

    Added choice of decks to export (Deck Dock)

--- src/components/DeckExport.vue
+++ src/components/DeckExport.vue
@@ -5,6 +5,20 @@
     </v-row>
     <v-row>
       <v-col>
+        <h2>Choose the decks to export</h2>
+        <v-checkbox
+        v-for="deck in getDecks()"
+        v-model="selectedDecks"
+        :key="deck.name"
+        :label="deck.name"
+        :value="deck.name"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h2>Copy and save this string to save your decks</h2>
+      </v-col>
+      <v-col>
         <v-textarea
         readonly
         rows="4"
@@ -20,15 +34,26 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'DeckExport',
+  data: () => ({
+    selectedDecks: []
+  }),
   computed: {
-    export: function () {
-      return this.serialize()
-    }
+    export: function () { return this.serialize() }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.selectedDecks = this.getDecks().map(deck => deck.name)
+    })
   },
   methods: {
     ...mapGetters('decks', ['getDecks']),
+    decksToExport: function () {
+      return this.getDecks()
+        .filter(deck => this.selectedDecks
+          .includes(deck.name))
+    },
     serialize: function () {
-      return btoa(JSON.stringify(this.getDecks()))
+      return btoa(JSON.stringify(this.decksToExport()))
     }
   }
 }
